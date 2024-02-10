5a56d92e7522b972f60e1d26185888c9e20f5835
waltersimekha <waltersimekha@gmail.com>
2024-02-11 02:09:19 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import MovieCard from "./MovieCard";
 
 function App() {
   const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+  const posterURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+   
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title}  posterSrc={posterURL} genres={genresArr} />
     </div>
   );
 }
